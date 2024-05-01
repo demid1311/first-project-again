7395b4fb4f4a673c3ada4628b161e7549350e05f
demid1311 <drobyshevdd131111@gmail.com>
2024-05-01 16:58:51 +0300

    train

---
+++ Климыч/scenes/player.gd
@@ -0,0 +1,79 @@
+extends CharacterBody2D
+
+signal healthChanged
+
+@export var speed: int = 35
+@onready var animations = $AnimationPlayer
+@onready var effects = $Effects
+@onready var hurtTimer = $hurtTimer
+@onready var hurtBox = $hurtBox
+@export var maxHealth: int = 3
+@onready var currentHealth: int = maxHealth
+
+@export var knockbackPower: int = 300
+
+var isHurt: bool = false
+
+
+func _ready():
+	effects.play("RESET")
+
+func getInput():
+	var moveDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = moveDirection * speed
+	
+
+func updateAnimation():
+	if velocity.length() == 0:
+		animations.stop()
+	else: 
+		var direction = "Down"
+		if velocity.x < 0: direction = "Left"
+		elif velocity.x > 0: direction = "Right"
+		elif velocity.y < 0: direction = "Up"
+		animations.play("walk" + direction)
+
+
+func handleCollision():
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		var collider = collision.get_collider()
+
+func _physics_process(delta):
+	getInput()
+	move_and_slide()
+	updateAnimation()
+	handleCollision()
+	if !isHurt:
+		for area in hurtBox.get_overlapping_areas():
+			if area.name == "hitBox":
+				hurtByEnemy(area)
+
+func hurtByEnemy(area):
+	currentHealth -= 1
+	if currentHealth < 0:
+		currentHealth = maxHealth
+		
+	healthChanged.emit(currentHealth)
+	isHurt = true
+	knockback(area.get_parent().velocity)
+	effects.play("Моргание")
+	hurtTimer.start()
+	await hurtTimer.timeout
+	effects.play("RESET")
+	isHurt = false
+		
+func _on_hurt_box_area_entered(area):
+	if area.has_method("collect"):
+		area.collect()
+
+func knockback(enemyVelocity: Vector2):
+	var knockbackDirection = (enemyVelocity - velocity).normalized() * knockbackPower
+	velocity = knockbackDirection
+	move_and_slide()
+
+
+func _on_hurt_box_area_exited(area):
+	pass
+
+
